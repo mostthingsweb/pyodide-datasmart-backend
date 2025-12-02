811c3c7bbc8eac9849f190ff39072526e347b9b6
Chris Laplante <mostthingsweb@gmail.com>
2025-12-01 20:26:07 -0500

    add bitbake 5.2.4 as a zipfile and use zipimporter to load modules

--- src/entry.py
+++ src/entry.py
@@ -1,5 +1,27 @@
+import sys
+from unittest.mock import MagicMock
+
+sys.modules["fcntl"] = MagicMock(name="fcntl")
+sys.modules["codegen"] = MagicMock(name="codegen")
+
+from pathlib import Path, PurePosixPath
 from workers import Response, WorkerEntrypoint
-from submodule import get_hello_message
+import zipimport
+
+_BITBAKE_ZIP_FILE = Path(__file__).resolve().parent / "bundled" / "yocto-5.2.4.zip"
+imp = zipimport.zipimporter(str(_BITBAKE_ZIP_FILE / "bitbake-yocto-5.2.4" / "lib"))
+
+imp.load_module("bs4")
+bb = imp.load_module("bb")
+
+import bb.data_smart
+from bb.data_smart import DataSmart
+
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        return Response(get_hello_message())
+        try:
+            ds = DataSmart()
+            return Response(f"DataSmart import OK ({DataSmart.__module__}), obj={ds}")
+        except Exception as exc:
+            return Response(f"DataSmart import failed: {exc!r}\nsys.path={sys.path}")
+    
